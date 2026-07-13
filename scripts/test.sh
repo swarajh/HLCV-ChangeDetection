a77b72df99c643de1c0dcee0ae5b89596e8e4e63
Parthiv Sarkar <kakarot@Parthivs-MacBook-Pro-3.local>
2026-07-13 23:24:01 +0200

    Fix

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,7 +11,7 @@ DATA_PATH="datasets/LEVIR-CD/test"
 
 # Optional: If you want to test a specific checkpoint, set the path below.
 # If left empty (""), the python script will automatically fetch the best one from the CSV!
-CHECKPOINT_PATH="" 
+CHECKPOINT_PATH="checkpoints/swin_simsiam_20260713_204422.pth" 
 # Example: CHECKPOINT_PATH="checkpoints/swin_baseline_20231024_120000.pth"
 # ==========================================
 
