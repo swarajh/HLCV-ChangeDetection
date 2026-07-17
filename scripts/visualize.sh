fc6df3b06653596c6ee1d813668c4a0e080aa285
Parthiv Sarkar <kakarot@Parthivs-MacBook-Pro-3.local>
2026-07-17 23:36:48 +0200

    Generating Results

--- scripts/visualize.sh
+++ scripts/visualize.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-MODEL_TYPE="sim"           # Options: "mim", "simsiam", "sim", "baseline"
+MODEL_TYPE="simsiam"           # Options: "mim", "simsiam", "sim", "baseline"
 
 # Path to the test dataset
 DATA_PATH="datasets/LEVIR-CD/test"
 
-CHECKPOINT_PATH="checkpoints/swin_sim_20260717_142056.pth" 
+CHECKPOINT_PATH="vit_base_patch14_dinov2.lvd142m_simsiam_lora_20260716_154943.pth" 
 
 MODEL_NAME="swin"
 # Path definitions
